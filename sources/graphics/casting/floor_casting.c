d4219f6e72cb91120b87abcc398e830273b231a4
Nicolas PLIEGER <plieger05@gmail.com>
2023-07-13 14:52:19 +0200

    Normed floor_casting

--- sources/graphics/casting/floor_casting.c
+++ sources/graphics/casting/floor_casting.c
@@ -6,90 +6,107 @@
 /*   By: emis <emis@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 16:25:33 by emis              #+#    #+#             */
-/*   Updated: 2023/07/10 14:16:44 by nplieger         ###   ########.fr       */
+/*   Updated: 2023/07/13 14:51:08 by nplieger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "graphics.h"
 
+typedef struct rc_floor
+{
+	float	screen_center;
+	float	horizon;
+	float	ray_dir_x0;
+	float	ray_dir_y0;
+	float	ray_dir_x1;
+	float	ray_dir_y1;
+	float	row_distance;
+	float	floor_step_x;
+	float	floor_step_y;
+	float	floor_x;
+	float	floor_y;
+	int		texture_x;
+	int		texture_y;
+}	t_rc_floor;
+
+static void	update_projection_variables(t_gui *gui, t_rc_floor *rc,
+	size_t height)
+{
+	rc->ray_dir_x0 = (gui->cam.dir.x * gui->cam.zoom) - gui->cam.plane.x;
+	rc->ray_dir_y0 = (gui->cam.dir.y * gui->cam.zoom) - gui->cam.plane.y;
+	rc->ray_dir_x1 = (gui->cam.dir.x * gui->cam.zoom) + gui->cam.plane.x;
+	rc->ray_dir_y1 = (gui->cam.dir.y * gui->cam.zoom) + gui->cam.plane.y;
+	rc->row_distance = height - rc->horizon;
+	if (rc->row_distance != 0)
+	{
+		if (height > rc->horizon)
+			rc->row_distance = rc->screen_center / rc->row_distance;
+		else
+			rc->row_distance = rc->screen_center / -rc->row_distance;
+	}
+	rc->floor_step_x = rc->row_distance * (rc->ray_dir_x1 - rc->ray_dir_x0)
+		/ SCRWIDTH;
+	rc->floor_step_y = rc->row_distance * (rc->ray_dir_y1 - rc->ray_dir_y0)
+		/ SCRWIDTH;
+}
+
+static void	set_texture_coordinates(t_gui *gui, t_rc_floor *rc)
+{
+	int	cell_x;
+	int	cell_y;
+
+	cell_x = (int)rc->floor_x;
+	cell_y = (int)rc->floor_y;
+	rc->texture_x = (int)(gui->textures.width * (rc->floor_x - cell_x))
+		& (gui->textures.width - 1);
+	rc->texture_y = (int)(gui->textures.height * (rc->floor_y - cell_y))
+		& (gui->textures.height - 1);
+}
+
+static void	draw_pixel(t_gui *gui, t_rc_floor *rc, size_t x, size_t y)
+{
+	int	color;
+
+	if (y > rc->horizon)
+	{
+		if (gui->textures.floorceil && gui->textures.floorceil[0])
+			color = pixget(gui->textures.floorceil[0], gui->textures.width
+					* rc->texture_y + rc->texture_x, 0);
+		else
+			color = gui->textures.floor_color;
+	}
+	else
+	{
+		if (gui->textures.floorceil && gui->textures.floorceil[1])
+			color = pixget(gui->textures.floorceil[1], gui->textures.width
+					* rc->texture_y + rc->texture_x, 0);
+		else
+			color = gui->textures.ceil_color;
+	}
+	color = (color >> 1) & 8355711;
+	pixput(gui->buffer, x, y, color);
+}
+
 void	floor_cast(t_gui *gui)
 {
-	size_t	y;
+	t_rc_floor	rc;
+	size_t		x;
+	size_t		y;
 
-
-	// Center of screen (horizon
-	float	screen_center = 0.5 * SCRHEIGHT;
-	// Player horizon
-	float	horizon = screen_center + gui->cam.pitch;
-
+	rc.screen_center = 0.5 * SCRHEIGHT;
+	rc.horizon = rc.screen_center + gui->cam.pitch;
 	y = 0;
 	while (y < SCRHEIGHT)
 	{
-		// rayDir for leftmost ray (x = 0) and rightmost ray (x = w)
-		float rayDirX0 = (gui->cam.dir.x * gui->cam.zoom) - gui->cam.plane.x;
-		float rayDirY0 = (gui->cam.dir.y * gui->cam.zoom) - gui->cam.plane.y;
-		float rayDirX1 = (gui->cam.dir.x * gui->cam.zoom) + gui->cam.plane.x;
-		float rayDirY1 = (gui->cam.dir.y * gui->cam.zoom) + gui->cam.plane.y;
-
-		// Current y position centered on horizon
-		int		p = y - horizon;
-
-		// -1 to 1. Distance current Y from the player's horizon
-		float	row_distance;
-		if (p && y > horizon)
-			row_distance = screen_center / p;
-		else if (p && y <= horizon)
-			row_distance = screen_center / -p;
-		else
-			row_distance = 0;
-
-		// calculate the real world step vector we have to add for each x (parallel to camera plane)
-		// adding step by step avoids multiplications with a weight in the inner loop
-		float floor_step_x = row_distance * (rayDirX1 - rayDirX0) / SCRWIDTH;
-		float floor_step_y = row_distance * (rayDirY1 - rayDirY0) / SCRWIDTH;
-
-		// real world coordinates of the leftmost column. This will be updated as we step to the right.
-		float floor_x = gui->cam.posi.x + row_distance * rayDirX0;
-		float floor_y = gui->cam.posi.y + row_distance * rayDirY0;
-
-		size_t	x;
-
 		x = 0;
+		update_projection_variables(gui, &rc, y);
+		rc.floor_x = gui->cam.posi.x + rc.row_distance * rc.ray_dir_x0;
+		rc.floor_y = gui->cam.posi.y + rc.row_distance * rc.ray_dir_y0;
 		while (x < SCRWIDTH)
 		{
-			// the cell coord is simply got from the integer parts of floorX and floorY
-			int cell_x = (int)(floor_x);
-			int cell_y = (int)(floor_y);
-
-			// get the texture coordinate from the fractional part
-			int tx = (int)(gui->textures.width * (floor_x - cell_x)) & (gui->textures.width - 1);
-			int ty = (int)(gui->textures.height * (floor_y - cell_y)) & (gui->textures.height - 1);
-
-			// Increment floor coord
-			floor_x += floor_step_x;
-			floor_y += floor_step_y;
-
-			int	color;
-
-			if (y > horizon)
-			{
-				// floor
-				if (gui->textures.floorceil && gui->textures.floorceil[0])
-					color = pixget(gui->textures.floorceil[0], gui->textures.width * ty + tx, 0);
-				else
-					color = gui->textures.floor_color;
-				color = (color >> 1) & 8355711; // make a bit darker
-			}
-			else
-			{
-				// ceiling
-				if (gui->textures.floorceil && gui->textures.floorceil[1])
-					color = pixget(gui->textures.floorceil[1], gui->textures.width * ty + tx, 0);
-				else
-					color = gui->textures.ceil_color;
-				color = (color >> 1) & 8355711; // make a bit darker
-			}
-			pixput(gui->buffer, x, y, color);
-
+			set_texture_coordinates(gui, &rc);
+			rc.floor_x += rc.floor_step_x;
+			rc.floor_y += rc.floor_step_y;
+			draw_pixel(gui, &rc, x, y);
 			x++;
 		}
 		y++;
