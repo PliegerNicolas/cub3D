ccb3a42ce55d36586ec65fadac8398048d631a12
PliegerNicolas <plieger05@gmail.com>
2023-08-13 12:14:42 +0200

    Hud didn't work well relative to compiler.

--- sources/graphics/hud.c
+++ sources/graphics/hud.c
@@ -6,37 +6,35 @@
 /*   By: emis <emis@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 18:18:56 by emis              #+#    #+#             */
-/*   Updated: 2023/08/13 12:03:11 by nicolas          ###   ########.fr       */
+/*   Updated: 2023/08/13 12:07:10 by nicolas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "graphics.h"
 
-static int color_angle(t_gui *gui, t_vect ori, t_vect pt)
+static int	color_angle(t_gui *gui, t_vect ori, t_vect pt)
 {
-    double alpha;
-    int color;
+	double	alpha;
+	int		color;
 
-    alpha = angle((t_vect){ori.x, -ori.y}, v_sub(pt, ori))
-            * (1 - 2 * (pt.x + pt.y < ori.x + ori.y));
-    color = (int)((255.0 * alpha) / M_PI);
-    if (color < 0 && (-color) < (255 - 255 * (gui->cam.stat.max[XP]
-			- gui->cam.stat.get[XP]) / gui->cam.stat.max[XP]))
-        color = MAPBLUE;
-    else if (color >= 0 && color < (85 - 85 * (gui->cam.stat.max[HP]
-			- gui->cam.stat.get[HP]) / gui->cam.stat.max[HP]))
-        color = MAPRED;
-    else if (color > 85 && color < (170 - 85 * (gui->cam.stat.max[ARM]
-			- gui->cam.stat.get[ARM]) / gui->cam.stat.max[ARM]))
-        color = MAPYEL;
-    else if (color > 170 && color > (170 + 85 * (gui->cam.stat.max[STA]
-			- gui->cam.stat.get[STA]) / gui->cam.stat.max[STA]))
-        color = MAPGREEN;
-    else
-        color = MAPBLACK;
-    return color;
+	alpha = angle((t_vect){ori.x, -ori.y}, v_sub(pt, ori))
+		* (1 - 2 * (pt.x + pt.y < ori.x + ori.y));
+	color = (int)((255.0 * alpha) / M_PI);
+	if (color < 0 && (-color) < (255 - 255 * (gui->cam.stat.max[XP]
+				- gui->cam.stat.get[XP]) / gui->cam.stat.max[XP]))
+		color = MAPBLUE;
+	else if (color >= 0 && color < (85 - 85 * (gui->cam.stat.max[HP]
+				- gui->cam.stat.get[HP]) / gui->cam.stat.max[HP]))
+		color = MAPRED;
+	else if (color > 85 && color < (170 - 85 * (gui->cam.stat.max[ARM]
+				- gui->cam.stat.get[ARM]) / gui->cam.stat.max[ARM]))
+		color = MAPYEL;
+	else if (color > 170 && color > (170 + 85 * (gui->cam.stat.max[STA]
+				- gui->cam.stat.get[STA]) / gui->cam.stat.max[STA]))
+		color = MAPGREEN;
+	else
+		color = MAPBLACK;
+	return (color);
 }
-
 
 static void	curvybars(t_gui *gui, t_vect where, int s)
 {
