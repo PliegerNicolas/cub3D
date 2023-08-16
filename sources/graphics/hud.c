1cfaf634dad81d4fd1bfb2bd16ebc74233b61d09
Nicolas PLIEGER <plieger05@gmail.com>
2023-08-16 17:09:37 +0200

    Added a toggle option for focus mode of minimap.

--- sources/graphics/hud.c
+++ sources/graphics/hud.c
@@ -6,7 +6,7 @@
 /*   By: emis <emis@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 18:18:56 by emis              #+#    #+#             */
-/*   Updated: 2023/08/16 16:05:40 by nplieger         ###   ########.fr       */
+/*   Updated: 2023/08/16 17:09:02 by nplieger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,13 @@ static int	color_angle(t_gui *gui, t_vect ori, t_vect pt)
 			- gui->cam.stat.get[XP]) / gui->cam.stat.max[XP])
 		color = MAPYEL - ((-color * 3 / 4) << 16) - (-color << 8);
 	else if (color >= 0 && (color < 85 - 85 * (gui->cam.stat.max[HP]
-			- gui->cam.stat.get[HP]) / gui->cam.stat.max[HP]))
+				- gui->cam.stat.get[HP]) / gui->cam.stat.max[HP]))
 		color = MAPRED;
 	else if ((color > 85) & (color < 170 - 85 * (gui->cam.stat.max[ARM]
-			- gui->cam.stat.get[ARM]) / gui->cam.stat.max[ARM]))
+				- gui->cam.stat.get[ARM]) / gui->cam.stat.max[ARM]))
 		color = MAPGREY;
 	else if ((color > 170) & (color > 170 + 85 * (gui->cam.stat.max[STA]
-			- gui->cam.stat.get[STA]) / gui->cam.stat.max[STA]))
+				- gui->cam.stat.get[STA]) / gui->cam.stat.max[STA]))
 		color = MAPBLUE;
 	else
 		color = MAPBLACK;
@@ -68,7 +68,8 @@ void	hud(t_gui *gui)
 	if (is_mask_set(&gui->cam.rndr, MINIMAP_CIRCULAR))
 	{
 		s = 14;
-		minimap(gui, (t_vect){s, s}, s, !(gui->keys & (1 << KP_map)));
+		minimap(gui, (t_vect){s, s}, s,
+			!is_mask_set(&gui->cam.rndr, MINIMAP_FOCUS));
 		curvybars(gui, (t_vect){0, 0}, s);
 	}
 	else
